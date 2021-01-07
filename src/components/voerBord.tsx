450b5a10d804a592ed1d2504df262831ed3cb9b0
lonkaars <l.leblansch@gmail.com>
2021-01-07 11:14:49 +0100

    tbody toegevoegd aan voerBord

--- src/components/voerBord.tsx
+++ src/components/voerBord.tsx
@@ -8,30 +8,32 @@ export function VoerBord(props: VoerBordProps) {
 		borderSpacing: 8,
 		width: "100%"
 	}}>
-	{
-		[...Array(props.height)].map(() => (
-			<tr>
-				{[...Array(props.width)].map(() => (
-					<td style={{
-						position: "relative",
-						width: "100%",
-						padding: 0
-					}}>
-						<div style={{
-							display: "block",
-							marginTop: "100%"
-						}}/>
-						<div style={{
-							position: "absolute",
-							top: 0, left: 0, right: 0, bottom: 0,
-							borderRadius: 6,
-							border: "2px solid var(--background-alt)",
-							opacity: .5
-						}}/>
-					</td>
-				))}
-			</tr>
-		))
-	}
+		<tbody>
+			{
+				[...Array(props.height)].map(() => (
+					<tr>
+						{[...Array(props.width)].map(() => (
+							<td style={{
+								position: "relative",
+								width: "100%",
+								padding: 0
+							}}>
+								<div style={{
+									display: "block",
+									marginTop: "100%"
+								}}/>
+								<div style={{
+									position: "absolute",
+									top: 0, left: 0, right: 0, bottom: 0,
+									borderRadius: 6,
+									border: "2px solid var(--background-alt)",
+									opacity: .5
+								}}/>
+							</td>
+						))}
+					</tr>
+				))
+			}
+		</tbody>
 	</table>
 }
