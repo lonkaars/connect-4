fd4c608f2cf61a3bc655cda49a1e2d0884f2a8fa
lonkaars <l.leblansch@gmail.com>
2021-01-15 11:53:01 +0100

    private token login

--- api/auth/login_token.py
+++ api/auth/login_token.py
@@ -2,6 +2,11 @@ from flask import Blueprint, request
 from main import cursor
 from auth.token import validate_token, hash_token
 
+def token_login(token):
+    hashed = hash_token({ "token": token, "expirationDate": 0 })
+    user_id = cursor.execute("select user_id from users where valid_tokens like ?", [f"%{hashed['token']}%"]).fetchone()
+    return None if not user_id else user_id
+
 token = Blueprint('token', __name__)
 
 @token.route('/token', methods = ['POST'])
@@ -11,9 +16,4 @@ def index():
     auth_token = data.get("token") or ""
     if not auth_token: return "", 400
 
-    hashed = hash_token({ "token": auth_token, "expirationDate": 0 })
-    user_id = cursor.execute("select user_id from users where valid_tokens like ?", [f"%{hashed['token']}%"]).fetchone()
-
-    if not user_id: return "", 401
-
-    return "", 200 if validate_token(user_id[0], auth_token) else 401
+    return "", 200 if token_login(auth_token) else 401
