ab1b3c4db3e90491c1f42910e85b1b830599a929
lonkaars <l.leblansch@gmail.com>
2021-02-06 11:36:45 +0100

    WIP account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from '../components/navbar';
 import { CenteredPage, PageTitle } from '../components/page';
 import { Vierkant } from '../components/ui';
+import { AccountAvatar } from '../components/account';
 
 export default function AccountPage() {
 	return (
@@ -9,7 +10,31 @@ export default function AccountPage() {
 			<CenteredPage width={802}>
 				<PageTitle>Profiel</PageTitle>
 				<Vierkant fullwidth>
-					Gert
+					<AccountAvatar size={128} dummy/>
+					<div style={{
+						display: "inline-block",
+						verticalAlign: "top",
+						marginLeft: 12,
+						width: "calc(100% - 128px - 12px)"
+					}}>
+						<h2 style={{ fontSize: 32 }}>Gebruikersnaam</h2>
+						<p style={{ marginTop: 6 }}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						</p>
+					</div>
+					<div style={{
+						position: "absolute",
+						backgroundColor: "var(--background)",
+						height: "40px",
+						bottom: 24, left: 24 + 12 + 128, right: 24
+					}}>
+						<div style={{
+							width: 40,
+							height: 30,
+							backgroundColor: "#ff00ff",
+							verticalAlign: "bottom"
+						}}></div>
+					</div>
 				</Vierkant>
 			</CenteredPage>
 		</div>
