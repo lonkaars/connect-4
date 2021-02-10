b2b96a7ba098d94c8701b15323c3a76684e51593
lonkaars <l.leblansch@gmail.com>
2021-02-10 09:56:55 +0100

    working loggedIn in navbar

--- components/recentGames.tsx
+++ components/recentGames.tsx
@@ -21,24 +21,26 @@ export default class RecentGames extends Component {
 		return <div>
 			<h2>Recente partijen</h2>
 			<table width="100%" style={{ marginTop: "16px", textAlign: "center" }}>
-				<tr>
-					<th style={{ width: "50%" }}>Tegenstander</th>
-					<th style={{ width: "20%" }}>Uitkomst</th>
-					<th style={{ width: "15%" }}>Zetten</th>
-					<th style={{ width: "15%" }}>Datum</th>
-				</tr>
-				<tr>
-					<td style={LeftAlignedTableColumn}>Naam hier</td>
-					<td style={{ color: "var(--disk-b-text)" }}>Gewonnen</td>
-					<td>7</td>
-					<td style={RightAlignedTableColumn}>Vandaag</td>
-				</tr>
-				<tr>
-					<td style={LeftAlignedTableColumn}>Nog meer namen</td>
-					<td style={{ opacity: .6 }}>Gelijkspel</td>
-					<td>42</td>
-					<td style={RightAlignedTableColumn}>Gisteren</td>
-				</tr>
+				<tbody>
+					<tr>
+						<th style={{ width: "50%" }}>Tegenstander</th>
+						<th style={{ width: "20%" }}>Uitkomst</th>
+						<th style={{ width: "15%" }}>Zetten</th>
+						<th style={{ width: "15%" }}>Datum</th>
+					</tr>
+					<tr>
+						<td style={LeftAlignedTableColumn}>Naam hier</td>
+						<td style={{ color: "var(--disk-b-text)" }}>Gewonnen</td>
+						<td>7</td>
+						<td style={RightAlignedTableColumn}>Vandaag</td>
+					</tr>
+					<tr>
+						<td style={LeftAlignedTableColumn}>Nog meer namen</td>
+						<td style={{ opacity: .6 }}>Gelijkspel</td>
+						<td>42</td>
+						<td style={RightAlignedTableColumn}>Gisteren</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	}
