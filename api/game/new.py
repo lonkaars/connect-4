5f391aadbcc818ab7fa13312fb2ee04ed02983c6
lonkaars <l.leblansch@gmail.com>
2021-02-12 18:56:52 +0100

    base /api/game/new

---
+++ api/game/new.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, make_response
+from main import cursor, connection
+from randid import new_uuid
+import time
+import json
+
+new_game = Blueprint('new', __name__)
+
+@new_game.route('/new', methods = ['POST'])
+def index():
+    data = request.get_json()
+
+    user_id = data.get("user_id") or "" # maybe set up a temporary user here?
+    game_settings = data.get("settings") or ""
+
+    if not user_id:
+        print("a temporary user should be set up here")
+
+    game_id = new_uuid("games")
+    timestamp = int( time.time() * 1000 )
+
+    cursor.execute("insert into games values (?, NULL, NULL, ?, NULL, NULL, ?, NULL, NULL, NULL, \"wait_for_opponent\", ?) ", (game_id, user_id, timestamp, json.dumps(game_settings)))
+    connection.commit()
+
+    return { "id": game_id }, 200
--- api/main.py
+++ api/main.py
@@ -14,10 +14,12 @@ from user.info import info
 from auth.signup import signup
 from auth.login import login
 from auth.login_token import token
+from game.new import new_game
 
 app.register_blueprint(status, url_prefix='/')
 app.register_blueprint(info, url_prefix='/user')
 app.register_blueprint(signup, url_prefix='/auth')
 app.register_blueprint(login, url_prefix='/auth')
 app.register_blueprint(token, url_prefix='/auth')
+app.register_blueprint(new_game, url_prefix='/game')
 
