c45d0923b65c7409dcb18554c3e1dd00324e3f92
lonkaars <l.leblansch@gmail.com>
2021-02-17 09:53:39 +0100

    working socket.io messages

--- api/main.py
+++ api/main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
 from status import status
 from user.info import info
@@ -10,7 +9,7 @@ from auth.signup import signup
 from auth.login import login
 from auth.login_token import token
 from game.new import new_game
-import game.socket
+from game.socket import run
 
 app.register_blueprint(status, url_prefix='/')
 app.register_blueprint(info, url_prefix='/user')
@@ -19,5 +18,4 @@ app.register_blueprint(login, url_prefix='/auth')
 app.register_blueprint(token, url_prefix='/auth')
 app.register_blueprint(new_game, url_prefix='/game')
 
-if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+if __name__ == "__main__": run(app)
