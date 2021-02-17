2cfb880bf17be2e73283e91cf11d4ad5b45b6ef6
lonkaars <l.leblansch@gmail.com>
2021-02-17 09:53:39 +0100

    working socket.io messages

--- api/game/socket.py
+++ api/game/socket.py
@@ -1,12 +1,30 @@
 from flask import Blueprint, request, make_response
-from flask_socketio import SocketIO, emit, disconnect, Namespace
-from main import socketio
+from flask_socketio import SocketIO, emit, disconnect, Namespace, emit
 import time
 import json
 
 class GameSocketNamespace(Namespace):
     def connect(self):
-        print("connection")
+        print("new connection")
+        emit("gert", {"gert": "banaan"})
 
-socketio.on_namespace(GameSocketNamespace("/game/socket"))
+    def on_connect(self):
+        print("new connection")
+        emit("gert", {"gert": "banaan"})
+
+    def on_disconnect(self):
+        print("disconnect")
+
+    def new_move(self, data):
+        print("new_move")
+        print(data)
+
+    def resign(self, data):
+        print("resign")
+        print(data)
+
+def run(app):
+    socketio = SocketIO(app)
+    socketio.on_namespace(GameSocketNamespace("/game/socket"))
+    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
 
