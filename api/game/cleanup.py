8cbd247d90c75c92ae45df458a29144c86ac5de7
lonkaars <l.leblansch@gmail.com>
2021-02-22 19:22:11 +0100

    fix stale game cleanup

--- api/game/cleanup.py
+++ api/game/cleanup.py
@@ -1,10 +1,13 @@
 from db import cursor, connection
 import threading
+import time
 
 def cleanup():
     now = int( time.time() * 1000 )
-    old_games = cursor.execute("select game_id from games where (status = \"wait_for_opponent\" or status = \"in_progress\") and last_activity < ?", [now - 5 * 60 * 1e3])
-    print(old_games)
+    old_games = cursor.execute("select game_id from games where (status = \"wait_for_opponent\" or status = \"in_progress\") and last_activity < ?", [now - 5 * 60 * 1e3]).fetchall()
+    for game_id in old_games:
+        cursor.execute("delete from games where game_id = ?", [game_id[0]])
+    connection.commit()
 
 def set_interval(func, sec): # https://stackoverflow.com/questions/2697039/python-equivalent-of-setinterval
     def func_wrapper():
@@ -15,3 +18,4 @@ def set_interval(func, sec): # https://stackoverflow.com/questions/2697039/pytho
     return t
 
 set_interval(cleanup, 5 * 60)
+
