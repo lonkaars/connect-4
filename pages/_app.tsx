7764b874e4894547f15f14f64a9c23a22e2dcc43
lonkaars <l.leblansch@gmail.com>
2021-02-23 08:20:59 +0100

    lettertype werkt :tada:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,15 @@ import '../styles/global.css';
 import '../styles/dark.css';
 import '../styles/disk.css';
 
+import Head from "next/head";
+
 export default function VierOpEenRijWebsite({ Component, pageProps }) {
-	return <Component {...pageProps}/>
+	return <div>
+		<Head>
+			<title>4 op een rij</title>
+			<link rel="stylesheet" href="/font/stylesheet.css"/>
+		</Head>
+		<Component {...pageProps}/>
+	</div>
 }
 
