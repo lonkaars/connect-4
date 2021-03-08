b56d17983facdbe29f397c08b7b14ac3ff783a88
lonkaars <l.leblansch@gmail.com>
2021-03-08 09:45:50 +0100

    user/password beginsels

---
+++ api/user/password.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request
+from db import cursor
+
+password = Blueprint('password', __name__)
+
+@password.route('/password')
+def index():
+    data = request.get_json()
+
+    if not data["password"] or \
+       not data["newPassword"]:
+        return "", 400
+
+    return {}, 200
+
+dynamic_route = ["/user", password]
+
