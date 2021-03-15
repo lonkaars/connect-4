b2e0569eb74cddc92c1e8576755137aa64f95d86
lonkaars <l.leblansch@gmail.com>
2021-03-15 21:52:08 +0100

    colors are right now v2

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,9 +3,9 @@ export function LogoDark() {
 		<div className="noclick">
 			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<rect width="24" height="24" fill="var(--background)"/>
-				<circle cx="6.5" cy="6.5" r="4.5" fill="var(--disk-a)"/>
-				<circle cx="6.5" cy="17.5" r="4.5" fill="var(--disk-b)"/>
-				<circle cx="17.5" cy="17.5" r="4.5" fill="var(--disk-a)"/>
+				<circle cx="6.5" cy="6.5" r="4.5" fill="var(--disk-b)"/>
+				<circle cx="6.5" cy="17.5" r="4.5" fill="var(--disk-a)"/>
+				<circle cx="17.5" cy="17.5" r="4.5" fill="var(--disk-b)"/>
 				<circle cx="17.5" cy="6.5" r="3.5" stroke="var(--text)" strokeWidth="2"/>
 			</svg>
 		</div>
@@ -17,9 +17,9 @@ export function LogoLight() {
 		<div className="noclick">
 			<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<rect width="24" height="24" fill="var(--page-background)"/>
-				<circle cx="6.5" cy="6.5" r="4.5" fill="var(--disk-a)"/>
-				<circle cx="6.5" cy="17.5" r="4.5" fill="var(--disk-b)"/>
-				<circle cx="17.5" cy="17.5" r="4.5" fill="var(--disk-a)"/>
+				<circle cx="6.5" cy="6.5" r="4.5" fill="var(--disk-b)"/>
+				<circle cx="6.5" cy="17.5" r="4.5" fill="var(--disk-a)"/>
+				<circle cx="17.5" cy="17.5" r="4.5" fill="var(--disk-b)"/>
 				<circle cx="17.5" cy="6.5" r="3.5" stroke="var(--background)" strokeWidth="2"/>
 			</svg>
 		</div>
