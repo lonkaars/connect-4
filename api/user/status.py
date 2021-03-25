a73ef14133d500a61ef9e1c5d7f49ebb6529a23f
lonkaars <l.leblansch@gmail.com>
2021-03-25 21:50:57 +0100

    smol fix

--- api/user/status.py
+++ api/user/status.py
@@ -7,12 +7,12 @@ status = Blueprint('user_status', __name__)
 
 @status.route('/status', methods = ['POST'])
 @auth_required("user")
-def index():
+def index(user_id):
     data = request.get_json()
     status = data.get("status") or ""
     if not status: return "", 400
 
-    cursor.execute("update users set status = ? where user_id = ?", [status[0:200], login])
+    cursor.execute("update users set status = ? where user_id = ?", [status[0:200], user_id])
     connection.commit()
 
     return "", 200
