1117131e8efc528e5d6dc225e9346e2cb2b459f2
lonkaars <l.leblansch@gmail.com>
2021-03-27 10:43:16 +0100

    lil fix

--- api/auth/login_token.py
+++ api/auth/login_token.py
@@ -5,7 +5,7 @@ from auth.token import validate_token, hash_token
 def token_login(token):
     hashed = hash_token({ "token": token, "expirationDate": 0 })
     user_id = cursor.execute("select user_id from users where valid_tokens like ?", [f"%{hashed['token']}%"]).fetchone()
-    return None if len(user_id) == 0 else user_id[0]
+    return None if not user_id else user_id[0]
 
 token = Blueprint('token', __name__)
 
